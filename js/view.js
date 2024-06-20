5535c647b0f5462ce6e43359c5a7afbec8a1ee81
sswethamohan <113842458+sswethamohan@users.noreply.github.com>
2024-06-20 16:51:21 +0530

    Add files via upload

---
+++ js/view.js
@@ -0,0 +1,14 @@
+<p id="name"></p>
+<p id="adhar"></p>
+              <p id="age"></p>
+              <p id="doses"></p>
+              <button type="button" onclick="Sumbit()"></button>
+              <script>
+                function Sumbit()
+                {
+
+                  let name = document.getElementById("name").value
+                  document.getElementById("name").innerHTML = `Name: ${exampleInputEmail1}`
+        
+                }
+              </script>
